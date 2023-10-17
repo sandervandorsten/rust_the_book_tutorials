44a26703a4e86c6b0218dbd76da283001d25a545
Sander van Dorsten <sander.vandorsten@digital-power.com>
2023-10-17 12:33:17 +0200

    feat(chapter 6): added enum example

---
+++ enums/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    enum IpAddr {
+        V4(String),
+        V6(String),
+    }
+
+    let _home = IpAddr::V4(String::from("127.0.0.1"));
+
+    let _loopback = IpAddr::V6(String::from("::1"));
+}
